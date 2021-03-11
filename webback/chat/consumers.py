49df1d4f2a8cc5eac17b3368026273b345907386
Unknown <512117556@qq.com>
2021-03-11 19:51:20 +0800

    配置vue与django中channels

--- webback/chat/consumers.py
+++ webback/chat/consumers.py
@@ -2,45 +2,76 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 import json
 
+# class ChatConsumer(WebsocketConsumer):
+#     # 信息连接时
+#     def connect(self):
+#         self.room_name = self.scope['url_route']['kwargs']['room_name']
+#         self.room_group_name = 'chat_%s' % self.room_name
+
+#         # Join room group
+#         async_to_sync(self.channel_layer.group_add)(
+#             self.room_group_name,
+#             self.channel_name
+#         )
+
+#         self.accept()
+
+#     # 断开连接时
+#     def disconnect(self, close_code):
+#         # Leave room group
+#         async_to_sync(self.channel_layer.group_discard)(
+#             self.room_group_name,
+#             self.channel_name
+#         )
+
+#     # 信息接收时
+#     def receive(self, text_data):
+#         text_data_json = json.loads(text_data)
+#         message = text_data_json['message']
+
+#         # Send message to room group
+#         async_to_sync(self.channel_layer.group_send)(
+#             self.room_group_name,
+#             {
+#                 'type': 'chat_message',
+#                 'message': message
+#             }
+#         )
+
+#     # Receive message from room group
+#     def chat_message(self, event):
+#         message = event['message']
+
+#         # Send message to WebSocket
+#         self.send(text_data=json.dumps({
+#             'message': message
+#         }))
+
+
+
+
+
 class ChatConsumer(WebsocketConsumer):
+    # 信息连接时
     def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
-
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
-            self.channel_name
-        )
-
         self.accept()
 
+    # 断开连接时
     def disconnect(self, close_code):
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name,
-            self.channel_name
-        )
+        pass
 
-    # Receive message from WebSocket
+    # 信息接收时
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
-
-    # Receive message from room group
-    def chat_message(self, event):
-        message = event['message']
+        print(text_data_json)
+        # message = text_data_json['message']
+        # message = chat_code_to_msg(text_data_json['code'],text_data_json['msg'])
 
         # Send message to WebSocket
         self.send(text_data=json.dumps({
-            'message': message
+            'message': 'websocket!'
         }))
+        
+    # def chat_code_to_msg(code,msg):
+    #     if code == 100:
+    #         token = msg
--- webfront/src/components/wstest.vue
+++ webfront/src/components/wstest.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        123
+        <div id="input-send">
+            <textarea placeholder="请输入内容" v-model="sendmsg"></textarea>
+            <button>发送信息</button>
+        </div>
     </div>
 </template>
 
@@ -12,14 +15,67 @@ export default {
             sendmsg:"",
         }
     },
-    created() {
-        this.$axios.get('/api/city1/').then(response => {
-            this.config1 = response.data.data
-        })
-        this.$axios.get('/api/city2/').then(response => {
-            this.config2 = response.data.data
-        })
+    mounted() {
+        this.initWebSocket()
     },
+    methods: {
+        initWebSocket() {
+            let wsuri = 'ws://127.0.0.1:8000/chat/lobby';
+            // 连接服务器
+            this.ws = new WebSocket(wsuri);
+            // 指定事件回调
+            this.ws.onmessage = this.websocketOnMessage;
+            this.ws.onopen = this.websocketOnOpen;
+            this.ws.onerror = this.websocketOnError;
+            this.ws.onclose = this.websocketClose;
+        }, 
+        // 发送消息
+        sendWebSocketMsg(msg) {
+            this.ws.send(JSON.stringify(msg))
+            // this.ws.send(msg)
+        },
+
+        websocketOnMessage(e) {
+            //获取websocket推送的数据
+            // let msg = e.data
+            // console.log(msg)
+            console.log(e.data)
+            let message = JSON.parse(e.data)
+            if (message.code == 100) {
+                console.log("fjk")
+            }
+        },
+
+        websocketOnOpen(e) {
+            console.log(e)
+            let enterroom = {
+                code:100,
+                msg:'hahaha'
+            }
+            this.sendWebSocketMsg(enterroom)
+            // console.log('连接 websocket 成功')
+        },
+        
+        // 连接失败时重新连接
+        websocketOnError(e) {
+            // this.initWebSocket()
+            console.log(e)
+        },
+        // 断开链接后报错
+        websocketClose(e) {
+            console.log('断开连接', e.code + " " + e.reason + " " + e.wasClean);
+            //this.initWebSocket() //断开后重新连接
+        },
+    },
+
+    // created() {
+    //     this.$axios.get('/api/city1/').then(response => {
+    //         this.config1 = response.data.data
+    //     })
+    //     this.$axios.get('/api/city2/').then(response => {
+    //         this.config2 = response.data.data
+    //     })
+    // },
 };
 </script>
 
